7472c9c6d2fdb3b1cc33e45bf7a3f517ba629778
Terry Palomares <tpalomares18@gmail.com>
2018-03-04 23:22:55 -0700

    created example backend

---
+++ pet-shop-backend/app.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var app = express()
+
+// Set up AWS DDB connection
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-west-2'});
+var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2018-02-08'});
+
+var petShopTable = 'pet-shop-table'
+
+var params = {
+  TableName: petShopTable,
+};
+
+app.get('/', function (req, res) {
+  docClient.scan(params, function(err, data) {
+	  if (err) {
+	    res.send(error)
+	  } else {
+	  	res.send(data.Items)
+	  }
+  });
+})
+
+app.listen(3000)
